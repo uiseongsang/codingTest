8afe777e97587b6a39211f6286c3a9dae0c504ac
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-10-11 23:55:36 +0900

    1929_소수구하기기

---
+++ 수학/1929_소수구하기.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,m;
+
+bool check(int n) {
+    if(n == 1) return 0;
+    if(n == 2) return 1;
+    if(n%2 == 0) return 0;
+
+    for(int i = 2; i * i <= n; i++) {
+        if(n%i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    cin >> n >> m;
+
+    for(int i = n; i <= m; i++) {
+        if(check(i) == 1) {
+            cout << i << "\n";
+        }
+    }
+    return 0;
+}
